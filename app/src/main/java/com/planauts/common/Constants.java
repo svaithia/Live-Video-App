ad151d64b005df03b421b3155bd74c13468b16a6
Saravanan Vaithianathan <svaithia@uwaterloo.ca>
2014-10-05 22:01:10 -0400

    Deleted constants

--- app/src/main/java/com/planauts/common/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.planauts.common;
-
-/**
- * Created by eight on 9/27/14.
- */
-public class Constants {
-    public static final String SECTION_API_URL = "http://services.dowjones.com/DowJones.mediaapi.WebHost/video/api/silo";
-    public static final String DEFAULT_CALLING_URL = "http://a1731.g.akamai.net/f/1731/67675/2m/video-api.wsj.com/mw2/mediarss/wsjdn/wsjtv.asp?type=playlist&query=Most+Viewed+WSJ+Videos";
-}
